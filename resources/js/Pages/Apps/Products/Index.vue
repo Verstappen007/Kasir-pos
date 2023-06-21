c3f2d133af3392ef5a743ba166b51cdfa04cb1f7
Rizkyyudistira12 <mrizkyyudistira@gmail.com>
2023-06-21 10:32:24 +0700

    Barcode Component

--- resources/js/Pages/Apps/Products/Index.vue
+++ resources/js/Pages/Apps/Products/Index.vue
@@ -34,7 +34,15 @@
                                     </thead>
                                     <tbody>
                                         <tr v-for="(product, index) in products.data" :key="index">
-                                            <td class="text-center">{{ product.barcode }}</td>
+                                            <td class="text-center">
+                                                <Barcode
+                                                    :value="product.barcode"
+                                                    :format="'CODE39'"
+                                                    :lineColor="'#000'"
+                                                    :width="1"
+                                                    :height="20"
+                                                />
+                                            </td>
                                             <td>{{ product.title }}</td>
                                             <td>Rp. {{ formatPrice(product.buy_price) }}</td>
                                             <td>Rp. {{ formatPrice(product.sell_price) }}</td>
@@ -75,6 +83,9 @@
     //import sweet alert2
     import Swal from 'sweetalert2';
 
+    //import component barcode
+    import Barcode from '../../../Components/Barcode.vue';
+
     export default {
         //layout
         layout: LayoutApp,
@@ -84,6 +95,7 @@
             Head,
             Link,
             Pagination,
+            Barcode
         },
 
         //props
